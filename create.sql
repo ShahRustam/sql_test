5f9d8969b63cf96af226edfccc4038d13d882a3c
rustam <Shahrustam98@gmail.com>
2017-04-08 16:56:01 +0300

    Add create script

---
+++ create.sql
@@ -0,0 +1,64 @@
+CREATE TABLE Department (
+  DepartmentID  INTEGER PRIMARY KEY AUTOINCREMENT,
+  Name          TEXT    NOT NULL,
+  Budget        INTEGER    NOT NULL,
+  Administrator TEXT    NOT NULL,
+  StartDate     DATE    NOT NULL
+);
+
+CREATE TABLE Course (
+  CourseID     INTEGER PRIMARY KEY AUTOINCREMENT,
+  Title        TEXT    NOT NULL,
+  Credits      TEXT    NOT NULL,
+  DepartmentID INTEGER NOT NULL,
+  FOREIGN KEY (DepartmentID) REFERENCES Department (DepartmentID)
+);
+
+CREATE TABLE OnlineCourse (
+  CourseID INTEGER NOT NULL PRIMARY KEY,
+  URL      TEXT    NOT NULL,
+  FOREIGN KEY (CourseID) REFERENCES Course (CourseID)
+);
+
+CREATE TABLE OnsiteCourse (
+  CourseID INTEGER NOT NULL PRIMARY KEY,
+  Location TEXT    NOT NULL,
+  Days     INTEGER NOT NULL,
+  Time     TEXT    NOT NULL,
+  FOREIGN KEY (CourseID) REFERENCES Course (CourseID)
+);
+
+CREATE TABLE Person (
+  PersonID       INTEGER PRIMARY KEY AUTOINCREMENT,
+  LastName       TEXT    NOT NULL,
+  FirstName      TEXT    NOT NULL,
+  HireDate       DATE    NOT NULL,
+  EnrollmentDate DATE    NOT NULL
+);
+
+CREATE TABLE StudentGrade (
+  EnrollmentID INTEGER PRIMARY KEY AUTOINCREMENT,
+  CourseID     INTEGER NOT NULL,
+  StudentID    INTEGER NOT NULL,
+  Grade        TEXT    NOT NULL,
+  FOREIGN KEY (CourseID) REFERENCES Course (CourseID),
+  FOREIGN KEY (StudentID) REFERENCES Person (PersonID)
+);
+
+CREATE TABLE CourseInstructor (
+  CourseID INTEGER NOT NULL,
+  PersonID INTEGER NOT NULL,
+  FOREIGN KEY (CourseID) REFERENCES Course (CourseID),
+  FOREIGN KEY (PersonID) REFERENCES Person (PersonID),
+  PRIMARY KEY (CourseID,PersonID)
+);
+
+CREATE TABLE OfficeAssignment (
+  InstructorID INTEGER NOT NULL PRIMARY KEY,
+  Location     TEXT    NOT NULL,
+  TimeStamp    INTEGER NOT NULL,
+  FOREIGN KEY (InstructorID) REFERENCES Person (PersonID)
+);
+
+
+
